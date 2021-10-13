a998678203213dac38c23cffe776c563ffed7004
mst827 <k.s.m501196gmail.com>
2021-10-13 23:44:46 +0900

    topページ作成

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>朝会アプリ</h1>
+    <button @click="transition">スタート</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  methods:{
+    transition:function(){
+      this.$router.push('/Top')
+    }
+  }
+};
+</script>
+
+<style scoped>
+button {
+  border: 1px solid #333;
+  background-color: #333;
+  color: #fff;
+  padding: 10px 20px;
+  margin-top: 10px;
+}
+</style>
---
+++ src/views/Top.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="top">
+    <button @click="contact">今日の担当者</button>
+    <h1>{{ member }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "top",
+  data: function() {
+    return {
+      members: [
+        "しみず",
+        "いとぐち",
+        "きむら",
+        "えがみ",
+        "ふじむら",
+        "なかむら",
+        "よしおか",
+        "すぎた",
+      ],
+      member: "",
+    };
+  },
+
+  methods: {
+    contact: function() {
+      let getData = Math.floor(Math.random() * this.members.length);
+      // let getData = choose_at_random(this.member);
+      this.member = this.members[getData];
+    },
+  },
+};
+</script>
